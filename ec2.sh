027216475fb55cb51ae69ef150dd88148811bd8c
shivapaladugula <81111233+shivapaladugula@users.noreply.github.com>
2022-10-26 15:03:51 +0530

    Create ec2.sh

---
+++ ec2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sh '--image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name AWSCLI --security-group-ids sg-0b32ee0857e50fa7b --subnet-id subnet-063c19e82edf33133'
